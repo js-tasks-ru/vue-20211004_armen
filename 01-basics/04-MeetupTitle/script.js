6dc4781e3f49f36bd35eaac3a57fe4150adbee7b
Armen <tarlamazyan@gmail.com>
2021-10-16 16:38:14 +0300

    master: MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,27 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+createApp({
+  data() {
+    return {
+      currentId: 1,
+      currentData: null,
+      currentMeetup: null,
+    };
+  },
+  watch: {
+    currentId(currentValue) {
+      this.currentId = currentValue;
+    },
+  },
+  mounted() {
+    fetchMeetupById(this.currentId).then((res) => {
+      this.currentMeetup = res.title;
+    });
+  },
+  updated() {
+    fetchMeetupById(this.currentId).then((res) => {
+      this.currentMeetup = res.title;
+    });
+  },
+}).mount('#app');
